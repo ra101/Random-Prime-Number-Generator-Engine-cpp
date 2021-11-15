6a899ef38a5f6ca9852f7aeab93dbbcd3eba8f5c
Parth Agarwal <agarwal.parth.101@protonmail.com>
2021-11-15 20:39:44 +0530

    linting

--- main.cpp
+++ main.cpp
@@ -1,40 +1,40 @@
 #include "prime.h"
-#include <sstream>          //complete std:stringstream
-#include <iostream>         
-#include <chrono>           //for timeStamp
-#include <random>           //random_prime_engine takes std::random engins as input template
+
+#include <iostream>
+#include <sstream> //complete std:stringstream
+#include <chrono>  //for timeStamp
+#include <random>  //random_prime_engine takes std::random engins as input template
 
 int main()
 {
-    ra::random_prime_engine<std::default_random_engine> rpe1;
+    ra::random_prime_engine<std::default_random_engine> rpe_1;
 
     std::chrono::steady_clock::time_point timeStamp = std::chrono::steady_clock::now();
-    unsigned long rand_sedd = ((std::chrono::duration_cast<std::chrono::nanoseconds>(timeStamp.time_since_epoch()).count()));
-    ra::random_prime_engine<std::default_random_engine> rpe2(rand_sedd);
+    unsigned long rand_seed = std::chrono::duration_cast<std::chrono::nanoseconds>(timeStamp.time_since_epoch()).count();
+    ra::random_prime_engine<std::default_random_engine> rpe_2(rand_seed);
 
     std::stringstream state;
-    state<<rpe1; //save current state
+    state << rpe_1; //save current state
 
-
-    std::cout<<"\nfrom rpe1 (without seed): ";
-    for(int i=0;i<10;i++)
+    std::cout << "\nfrom rpe_1 (without seed): ";
+    for (int i = 0; i < 10; i++)
     {
-        std::cout<<rpe1()<<", ";
+        std::cout << rpe_1() << ", ";
     }
 
-    std::cout<<"\nfrom rpe2 (with timestamp as seed): ";
-    for(int i=0;i<10;i++)
+    std::cout << "\nfrom rpe_2 (with timestamp as seed): ";
+    for (int i = 0; i < 10; i++)
     {
-        std::cout<<rpe2()<<", ";
+        std::cout << rpe_2() << ", ";
     }
 
-    state>>rpe1; //restore old state
-    std::cout<<"\nfrom rpe1 (without seed) again: ";
-    for(int i=0;i<10;i++)
+    state >> rpe_1; //restore old state
+    std::cout << "\nfrom rpe_1 (without seed) again: ";
+    for (int i = 0; i < 10; i++)
     {
-        std::cout<<rpe1()<<", ";
+        std::cout << rpe_1() << ", ";
     }
 
-    std::cout<<"\nMin of any rpe: "<<rpe1.min();
-    std::cout<<"\nMax of any rpe: "<<rpe1.max();
+    std::cout << "\nMin of any rpe: " << rpe_1.min();
+    std::cout << "\nMax of any rpe: " << rpe_1.max();
 }
